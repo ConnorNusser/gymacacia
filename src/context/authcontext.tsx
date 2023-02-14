a5550a0b216e637569d85cc141cff9dd1d159be7
Connor Nusser <connornusser@gmail.com>
2023-02-13 19:45:26 -0600

    fix some auth provider stuff

--- src/context/authcontext.tsx
+++ src/context/authcontext.tsx
@@ -1,10 +1,20 @@
 import { useContext, createContext} from "react";
+import { GoogleAuthProvider, 
+        signInWithPopup, 
+        signInWithRedirect,
+        signOut,
+        onAuthStateChanged } from "firebase/auth";
+import { auth } from "@/services/firebase";
 
 const AuthContext = createContext();
 
-export const AuthContextProvider = (children: any) => {
+export const AuthContextProvider = ({children}: any) => {
+    const googleSignIn = () => {
+        const provider = new GoogleAuthProvider();
+        signInWithPopup(auth, provider);
+    }
     return (
-        <AuthContext.Provider value = {{}}>
+        <AuthContext.Provider value = {{googleSignIn}}>
             {children}
         </AuthContext.Provider>
     )
