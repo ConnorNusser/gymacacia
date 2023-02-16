7b2ec83245d87c33ee4fe818be0bb9c6b0c81126
Connor Nusser <connornusser@gmail.com>
2023-02-16 01:23:55 -0600

    add in stuff

--- src/components/cardSearch.tsx
+++ src/components/cardSearch.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import search from '../styles/search.module.css';
-import SearchIcon from '@mui/icons-material/Search';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import LinkSocialMedia from './linkSocialMedia';
 function CardSearch() {
   return (
     <div>
     <div className={search.wrap}>
       <div className={search.search}>
-        <input type="text" className={search.searchTerm} placeholder="What are you looking for?"></input>
+        <input type="text" className={search.searchTerm} placeholder="Type some key words to create a post."></input>
         <button type="submit" className={search.searchButton}>
-          <SearchIcon />
+          < ArrowUpwardIcon/>
         </button>
       </div>
     </div>
--- src/components/linkSocialMedia.tsx
+++ src/components/linkSocialMedia.tsx
@@ -1,23 +1,35 @@
 import cardStyle from '../styles/card.module.css'
+enum IconLinks{
+    Instagram = "https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png",
+    Facebook = "https://upload.wikimedia.org/wikipedia/commons/9/91/036-facebook.png",
+    Twitter = "https://upload.wikimedia.org/wikipedia/commons/4/4f/Twitter-logo.svg"
 
-function LinkSocialMedia() {
+}
+interface ILinkSocialMedia{
+    imgLink: string;
+    name: any;
+}
+const LinkSocialMedia = (props: ILinkSocialMedia) =>{
     return (
         <li className={cardStyle.card}>
-		<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/psychopomp-100.jpg" alt="Psychopomp" />
-		<a className={cardStyle.cardImage} href="https://michellezauner.bandcamp.com/album/psychopomp-2" target="_blank">
-			<h2>Psychopomp</h2>
-			<p>Japanese Breakfast</p>
+		<img src= {props.imgLink} width="50" height="50" alt="Instagram" />
+		<a className={cardStyle.cardImage} href="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png" target="_blank">
+            <small><p>{props.name}</p></small>
+			<small><p>Not Linked</p></small>
 		</a>
 	    </li>
       );
   }
 
 export default function SocialMediaList(){
+    const Instagram = IconLinks.Instagram;
+    const Facebook = IconLinks.Facebook;
+    const Twitter = IconLinks.Twitter;
     return (
         <ul className={cardStyle.cardList}>
-            <LinkSocialMedia/>
-            <LinkSocialMedia/>
-            <LinkSocialMedia/>
+            <LinkSocialMedia imgLink = {IconLinks.Instagram} name = "Instagram"  />
+            <LinkSocialMedia imgLink = {IconLinks.Facebook} name ="Facebook"/>
+            <LinkSocialMedia imgLink = {IconLinks.Twitter} name = "Twitter"/>
         </ul>
     );
 }
