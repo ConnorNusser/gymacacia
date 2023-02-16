effd5b35051a1d8c236d95ea8844bb3143eb3880
Connor Nusser <connornusser@gmail.com>
2023-02-16 03:19:09 -0600

    more c

--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -1,13 +1,29 @@
 import { UserAuth } from "@/context/authcontext";
 import styles from '@/styles/Home.module.css'
 
-export default function Profile() {
+interface IProfileComponent {
+  photoUrl:any;
+  displayName:any;
+  email:any;
+}
+
+function Profile() {
   const {user} = UserAuth();
   return (
-    <div className={styles.centerStacked}>
-        <img src= {user.photoURL} width="100" height="100"/>
-        <p className={styles.title}>{user.displayName}</p>
-        <p className={styles.title}>{user.email}</p>
+    <div>
+    { user == '' || user == null ? <h1>No Info</h1> : <ProfileComponent photoUrl={user.photoURL} displayName={user.displayName} email = {user.email}/> }
     </div>
     );
 }
+
+const ProfileComponent = (props:IProfileComponent) => {
+  return (
+  <div className={styles.centerStacked}>
+        <img src= {props.photoUrl} width="100" height="100"/>
+        <p className={styles.title}>{props.displayName}</p>
+        <p className={styles.title}>{props.email}</p>
+    </div>
+  );
+}
+
+export default Profile;
