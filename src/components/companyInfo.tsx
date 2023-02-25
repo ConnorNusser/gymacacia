2894a43d59c0037792f05a4b5400d7375721c2fe
Connor <connornusser@gmail.com>
2023-02-24 20:39:10 -0600

    lolololol

--- src/components/companyInfo.tsx
+++ src/components/companyInfo.tsx
@@ -2,6 +2,7 @@ import { Button } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 import Badge from 'react-bootstrap/Badge';
 import styles from '@/styles/profile.module.css';
+import { useState } from 'react';
 
 const handleSubmit = () => {
     console.log("hi");
@@ -10,15 +11,34 @@ function companyInfo(){
   return (
     <div className="card">
   <div className="card-header">
-    Company Info
+    Company Name: None
   </div>
   <div className="card-body">
-    <h5 className="card-title">Special title treatment</h5>
-    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+    <h5 className="card-title">Bio</h5>
+    <p className="card-text"><EditCompanyBio/></p>
+    <Button className="btn btn-success">Edit</Button>
   </div>
 </div>
   );
+}
+
+function EditCompanyBio(){
+    const [text, setText] = useState(""); // initialize state with empty string
+  
+    const handleTextChange = (event: { target: { value: any; }; }) => {
+      setText(event.target.value); // update state with the new value of the textbox
+    };
+  
+    const handleSaveClick = () => {
+      // save the text somewhere, such as in a database or in local storage
+      console.log("Saving text:", text);
+    };
+    return (
+    <div>
+      <input type="text" value={text} onChange={handleTextChange} />
+      <button onClick={handleSaveClick}>Save</button>
+    </div>
+    );
 }
 
 export default companyInfo;
